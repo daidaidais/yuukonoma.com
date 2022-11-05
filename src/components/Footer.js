6655b6475411c6f7ab49c1b6d823810244835024
diceky <daisuke4yukita@gmail.com>
2022-11-05 23:04:00 +0900

    remove Feather icons credits and update CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,23 +4,8 @@ import { Row, Col } from "react-bootstrap";
 const Footer = ({ popIn }) => {
   return (
     <Row className="section-footer pop-in">
-      <Col sm={2} className="footer-text-wrapper">
-        <p className="footer-credits">
-          Icons by Cole Bemis from{" "}
-          <a
-            href="https://github.com/feathericons/feather"
-            className="credits-link"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Feather
-          </a>
-        </p>
-      </Col>
-      <Col sm={8} className="footer-text-wrapper">
+      <Col sm={{ span: 8, offset: 2 }} className="footer-text-wrapper">
         <p className={popIn ? "footer-text" : "footer-text-gone"}>ナマステ</p>
-      </Col>
-      <Col sm={2}>
         <p className="footer-copyright">&copy; Yuuko Noma</p>
       </Col>
     </Row>
